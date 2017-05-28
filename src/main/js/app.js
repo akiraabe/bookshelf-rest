ca52f325c790137e2711ad8eed269ec7b4414493
akiraabe <akiraabe2005@yahoo.co.jp>
2017-05-29 00:02:33 +0900

    Implement create, remove feature.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,59 +4,258 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
 
+const follow = require('./follow');
+
+const root = '/api';
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {books: []};
+        this.state = {books: [], attributes: [], pageSize: 2, links: {}};
+        this.updatePageSize = this.updatePageSize.bind(this);
+        this.onCreate = this.onCreate.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.onNavigate = this.onNavigate.bind(this);
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'books', params: {size: pageSize}}]
+        ).then(bookCollection => {
+            return client({
+                method: 'GET',
+                path: bookCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return bookCollection;
+            });
+        }).done(bookCollection => {
+            this.setState({
+                books: bookCollection.entity._embedded.books,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: bookCollection.entity._links});
+        });
+    }
+
+    onCreate(newBook) {
+        follow(client, root, ['books']).then(bookCollection => {
+            return client({
+                method: 'POST',
+                path: bookCollection.entity._links.self.href,
+                entity: newBook,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'books', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            if (typeof response.entity._links.last != "undefined") {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
+    }
+
+    onDelete(book) {
+        client({method: 'DELETE', path: book._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(bookCollection => {
+            this.setState({
+                books: bookCollection.entity._embedded.books,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: bookCollection.entity._links
+            });
+        });
+    }
+
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
     }
 
     // The API invoked after React renders a component in the DOM.
     componentDidMount() {
-        client({method: 'GET', path: '/api/books'}).done(response => {
-            this.setState({books: response.entity._embedded.books});
-        });
+        this.loadFromServer(this.state.pageSize);
     }
 
     // The API to "draw" the component on the screen.
     render() {
         return (
-            <BookList books={this.state.books}/>
+            <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <BookList books={this.state.books}
+                          links={this.state.links}
+                          pageSize={this.state.pageSize}
+                          onNavigate={this.onNavigate}
+                          onDelete={this.onDelete}
+                          updatePageSize={this.updatePageSize}/>
+            </div>
+        )
+    }
+}
+
+class CreateDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newBook = {};
+        this.props.attributes.forEach(attribute => {
+            newBook[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newBook);
+
+        // clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it.
+        window.location = "#";
+    }
+
+    render() {
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field"/>
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createBook">Create</a>
+                <div id="createBook" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+                        <h2> Create new Book</h2>
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
 
 class BookList extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.handleNavFirst = this.handleNavFirst.bind(this);
+        this.handleNavPrev = this.handleNavPrev.bind(this);
+        this.handleNavNext = this.handleNavNext.bind(this);
+        this.handleNavLast = this.handleNavLast.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    // tag::handle-nav[]
+    handleInput(e) {
+        e.preventDefault();
+        var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            ReactDOM.findDOMNode(this.refs.pageSize).value =
+                pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+
+    handleNavFirst(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
+    }
+    // end::handle-nav[]
+
     render() {
         var books = this.props.books.map(book =>
-            <Book key={book._links.self.href} book={book} />
+            <Book key={book._links.self.href} book={book} onDelete={this.props.onDelete}/>
         );
+
+        var navLinks = [];
+        if ("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if ("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        }
+        if ("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        }
+        if ("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        }
         return (
+            <div>
+                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
                 <table>
                     <tbody>
-                        <tr>
-                            <th>Title</th>
-                            <th>著者</th>
-                            <th>Publisher</th>
-                            <th>PublishDate</th>
-                        </tr>
-                        {books}
+                    <tr>
+                        <th>Title</th>
+                        <th>著者</th>
+                        <th>Publisher</th>
+                        <th>PublishDate</th>
+                    </tr>
+                    {books}
                     </tbody>
                 </table>
-            )
+                <div>
+                    {navLinks}
+                </div>
+            </div>
+        )
     }
 }
 
 class Book extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    handleDelete() {
+        this.props.onDelete(this.props.book);
+    }
     render() {
-        return(
+        return (
             <tr>
                 <td>{this.props.book.title}</td>
                 <td>{this.props.book.author}</td>
                 <td>{this.props.book.publisher}</td>
                 <td>{this.props.book.publishDate}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
         )
     }
